89aa9af3eb100c5062edc96e80ebe79efdba9047
reginafcompton <reginafcompton@gmail.com>
2017-03-20 16:54:48 -0500

    Improve everything

--- config.py
+++ config.py
@@ -1,3 +1,37 @@
-REDIS_QUEUE_KEY='metro_merger'
+REDIS_QUEUE_KEY = 'metro_merger'
 
-DEBUG=True
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+
+    'formatters': {
+        'all': {
+            'format': '[%(asctime)s][%(levelname)s] '
+                      '%(filename)s:line:%(lineno)d | %(message)s',
+            'datefmt': "%B %d, %Y - %H:%M:%S",
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+            'formatter': 'all'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'all',
+        },
+    },
+
+    'loggers': {
+        'tasks': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+            'propagate': False,
+            },
+    }
+}
+
+DEBUG = True
