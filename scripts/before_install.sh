a557e4d75938b1221cc17b95cbddfe0697891ffd
Eric van Zanten <eric.vanzanten@gmail.com>
2017-08-31 15:12:52 -0500

    Remove first

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 # Make directory for project
+rm -Rf /home/datamade/metro-pdf-merger
 mkdir -p /home/datamade/metro-pdf-merger
 
 cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/ && chown -R datamade.datamade . && sudo -H -u datamade blackbox_postdeploy
