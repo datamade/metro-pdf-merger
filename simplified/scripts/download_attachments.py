36751d4f60c9fc3c582aa920c345ae4c0ddf7070
hancush <hannah.cushman@gmail.com>
2021-10-21 10:17:51 -0500

    Add simplified pipeline outline

---
+++ simplified/scripts/download_attachments.py
@@ -0,0 +1,10 @@
+'''
+Download specified attachments into the attachments/ directory and return
+space-delimited list of attachment filenames, all with the PDF file extension.
+
+TBD: How to pass data from compile_pdfs management command to this container.
+Some options: https://www.astronomer.io/guides/airflow-passing-data-between-tasks
+'''
+import sys
+
+sys.stdout.write('attachments/foo.pdf attachments/bar.pdf')
