917132fc58f020cc6ea5c79c0f45ec12c46e7433
hancush <hannah.cushman@gmail.com>
2021-10-21 08:35:24 -0500

    Add memory limit to queue process

--- run_worker.py
+++ run_worker.py
@@ -1,3 +1,9 @@
+import resource
+
 from tasks import queue_daemon
 
+
+MAX_VIRTUAL_MEMORY = 150 * 1024 * 1024
+soft, hard = resource.getrlimit(resource.RLIMIT_AS)
+resource.setrlimit(resource.RLIMIT_AS, (MAX_VIRTUAL_MEMORY, hard))
 queue_daemon()
