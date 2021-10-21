705ff89582141ef90996934d46eba6eb0551bda1
hancush <hannah.cushman@gmail.com>
2021-10-21 10:22:35 -0500

    Expand comments

--- simplified/scripts/download_attachments.py
+++ simplified/scripts/download_attachments.py
@@ -2,6 +2,8 @@
 Download specified attachments into the attachments/ directory and return
 space-delimited list of attachment filenames, all with the PDF file extension.
 
+Can use scrapelib for retrying requests.
+
 TBD: How to pass data from compile_pdfs management command to this container.
 Some options: https://www.astronomer.io/guides/airflow-passing-data-between-tasks
 '''
